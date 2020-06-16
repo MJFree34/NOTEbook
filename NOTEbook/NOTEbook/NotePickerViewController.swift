15dfa537aa2ee6881c26bcbc3c8d23c250e27acf
MJFree34 <55849977+MJFree34@users.noreply.github.com>
2020-06-16 10:10:12 -0400

    Basic configuration

---
+++ NOTEbook/NOTEbook/NotePickerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  NOTEbook
+//
+//  Created by Matt Free on 6/16/20.
+//  Copyright © 2020 Matt Free. All rights reserved.
+//
+
+import UIKit
+
+class NotePickerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
