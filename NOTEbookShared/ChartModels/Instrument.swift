8d89c1124446fff785816e8a26807c1c3f19a33a
Matt Free <mjfree34@gmail.com>
2023-06-15 20:19:13 -0400

    Added starter files for basic functionality

--- NOTEbookShared/ChartModels/Instrument.swift
+++ NOTEbookShared/ChartModels/Instrument.swift
@@ -35,29 +35,10 @@ struct Instrument: Codable, Equatable {
     var type: InstrumentType
     var offset: Double
     
-    // UNCOMMENT WHEN OLD UI IS DELETED
     init(type: InstrumentType, offset: Double) {
         self.type = type
         self.offset = offset
     }
-    
-    // MARK: - FOR OLD UI DELETE WHEN REPLACED
-    var fingeringWidth: Int = 0
-    var chartCellHeight: Int = 0
-    var chartCenterOfStaffFromTop: Int = 0
-    var chartFingeringHeight: Int = 0
-    var maximumSpacingFingerings: Double = 0
-    
-    init(type: InstrumentType, fingeringWidth: Int, chartCellHeight: Int, chartCenterOfStaffFromTop: Int, offset: Double, chartFingeringHeight: Int, maximumSpacingFingerings: Double) {
-        self.type = type
-        self.fingeringWidth = fingeringWidth
-        self.chartCellHeight = chartCellHeight
-        self.chartCenterOfStaffFromTop = chartCenterOfStaffFromTop
-        self.offset = offset
-        self.chartFingeringHeight = chartFingeringHeight
-        self.maximumSpacingFingerings = maximumSpacingFingerings
-    }
-    // END OLD UI
 }
 
 extension Instrument: Hashable {
