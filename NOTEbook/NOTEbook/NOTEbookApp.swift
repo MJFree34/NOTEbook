0e5d61b918df5c079e8ba8baf41a4e68d9154119
Matt Free <mjfree34@gmail.com>
2023-06-15 20:07:41 -0400

    Initial commit with new Xcode project

---
+++ NOTEbook/NOTEbook/NOTEbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOTEbookApp.swift
+//  NOTEbook
+//
+//  Created by Matt Free on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct NOTEbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
